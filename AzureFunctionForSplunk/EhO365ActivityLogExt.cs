055660b170a159e3694ba0e37c237af2ba9a2b52
krzysiekokun <98360702+krzysiekokun@users.noreply.github.com>
2022-02-22 07:05:26 +0100

    Create EhO365ActivityLogExt.cs

---
+++ AzureFunctionForSplunk/EhO365ActivityLogExt.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionForSplunk
+{
+    public static class EhO365ActivityLogExt
+    {
+        [FunctionName("EhO365ActivityLogExt")]
+        public static async Task Run(
+            [EventHubTrigger("%input-hub-name-activity-log%", Connection = "hubConnection", ConsumerGroup = "%consumer-group-activity-log%")] string[] messages,
+            [EventHub("%output-hub-name-proxy%", Connection = "outputHubConnection")] IAsyncCollector<string> outputEvents,
+            IBinder blobFaultBinder,
+            IBinder incomingBatchBinder,
+            Binder queueFaultBinder,
+            ILogger log)
+        {
+            var runner = new Runner();
+            await runner.Run<O365AuditMessages, ActivityLogsSplunkEventMessages>(messages, blobFaultBinder, queueFaultBinder, incomingBatchBinder, outputEvents, log);
+        }
+    }
+}
